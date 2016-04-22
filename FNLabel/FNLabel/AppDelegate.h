50e8046bfc610667c9fa7250163d2cd18b347896
zyj <745972353@qq.com>
2016-04-22 15:38:52 +0800

    dfqwe

---
+++ FNLabel/FNLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FNLabel
+//
+//  Created by admin on 16/4/22.
+//  Copyright © 2016年 天涯海北. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FNLabel/FNLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FNLabel
+//
+//  Created by admin on 16/4/22.
+//  Copyright © 2016年 天涯海北. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
